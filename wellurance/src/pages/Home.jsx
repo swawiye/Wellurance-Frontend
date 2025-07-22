c4f5320ee33291476f70d7fff27d8a1b9f11e125
swawiye <sovereign.wawiye@gmail.com>
2025-07-22 11:20:10 +0300

    home page after log in

--- wellurance/src/pages/Home.jsx
+++ wellurance/src/pages/Home.jsx
@@ -90,10 +90,9 @@ function Home() {
                     <div className="space-y-4">
                         <h4 className="text-amber-600 font-semibold text-lg">Quick Links</h4>
                         <ul className="space-y-2">
-                            <li><a href="#" className="text-gray-300 hover:text-amber-600 transition">Home</a></li>
-                            <li><a href="#" className="text-gray-300 hover:text-amber-600 transition">Services</a></li>
-                            <li><a href="#" className="text-gray-300 hover:text-amber-600 transition">About Us</a></li>
-                            <li><a href="#" className="text-gray-300 hover:text-amber-600 transition">Contact</a></li>
+                            <li><a href="/" className="text-gray-300 hover:text-amber-600 transition">Home</a></li>
+                            <li><a href="/services" className="text-gray-300 hover:text-amber-600 transition">Services</a></li>
+                            <li><a href="/about" className="text-gray-300 hover:text-amber-600 transition">About Us</a></li>
                         </ul>
                     </div>
 
